ab51fd9d438bbe41ce398ddf1e09bdf165f704d9
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-06 17:24:18 -0500

    feat:methods are now it the right placement and our code is printing out.

--- src/Game.java
+++ src/Game.java
@@ -2,37 +2,29 @@ import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args){
-
+        System.out.println("The Quest for Baby Yoda's Coffeey");
         Scanner scanner = new Scanner(System.in);
-        String playerInput = scanner.nextLine();
+//        String playerInput = scanner.nextLine();
+        beginGame(scanner);
         gameContinues();
 
-        public static void beginGame(Scanner scanner){
-            System.out.println("Are you ready to begin your quest? (y/n)");
-            String playerReply = scanner.nextLine();
-            if (playerReply.equals("y")) {
-                System.out.println("Your quest starts now. ");
-            }
 
         }
 
-//        while (true) {
 
-//            System.out.println("Are you ready to begin?");
-//            String answer = scanner.nextLine();
-//            if (answer.toLowerCase().equals("yes")) {
-//                System.out.println("Your quest starts now.");
-//            }else{
-//                break;
-//            }
-
+    public static void beginGame(Scanner scanner){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Are you ready to begin your quest? (y/n)");
+        String playerReply = scanner.next();
+        if (playerReply.equals("y")) {
+            System.out.println("Your quest starts now. ");
         }
 
+    }
     public static void gameContinues(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("What is your name?");
         String playerName = scanner.nextLine();
 
-    }
-
+        }
 }
