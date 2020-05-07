60198bf5d6ae91ca07c2618321a9437a62685d4a
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-06 21:23:49 -0500

    feat: added storyline to game

--- src/Game.java
+++ src/Game.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args){
-        System.out.println("The Quest for Baby Yoda's Coffeey");
+        System.out.println("<=(⬤‿⬤)=＞" +
+                "The Quest for Baby Yoda's Coffee");
         Scanner scanner = new Scanner(System.in);
 //        String playerInput = scanner.nextLine();
         beginGame(scanner);
@@ -25,6 +26,15 @@ public class Game {
         Scanner scanner = new Scanner(System.in);
         System.out.println("What is your name?");
         String playerName = scanner.nextLine();
+        System.out.printf("Welcome, young master %s. We have much faith you will successfully accomplish your " +
+                        "mission.\n Baby Yoda is in disguise and its our job to safely get him passed storm trooper " +
+                "check points \n in and out of cities, the storm troopers are every where so you must be careful. \n " +
+                "Choose wisely whether to fight, hide, or run to save baby " +
+                "Yoda. \n Along the way you need to collect points to keep baby Yoda alive with coffee and frog " +
+                "biscuits.\n Good luck, young Padawan! May the force be with you.", playerName);
 
+        }
+        public static void firstQuestLevel (){
+            System.out.println("");
         }
 }
